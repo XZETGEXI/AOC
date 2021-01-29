0a26d77d2e8ba3344253823366ee2fc483798ce1
XZETGEXI <77068772+XZETGEXI@users.noreply.github.com>
2021-01-29 13:48:34 +0100

    Update d2.py

--- 2017/d2.py
+++ 2017/d2.py
@@ -1,34 +1,28 @@
 with open("d2.txt", "r") as f:
     data = f.readlines()
     data = [datum.strip().split("\t") for datum in data]
-    data = [map(int, datum) for datum in data]
+    data = [[*map(int, datum)] for datum in data]
 
 r = {}
 
 for n, datum in enumerate(data):
-    to_check = [*datum]
-    r[n] = max(to_check) - min(to_check)
+    r[n] = max(datum) - min(datum)
     
-print(sum(r.values()))
-
-with open("d2.txt", "r") as f:
-    data = f.readlines()
-    data = [datum.strip().split("\t") for datum in data]
-    data = [map(int, datum) for datum in data]
+print("silver", sum(r.values()))
 
 r = {}
 
 for n, datum in enumerate(data):
-    to_check = [*datum]
-    for i in to_check:
-        for j in to_check:
+    for i in datum:
+        for j in datum:
             if not i % j and i != j:
                 r[n] = (i,j)
                 continue
 
+
 o = 0
 
-for a,b in r.items():
+for b in r.values():
     o += (max(b) / min(b))
     
-print(o)
+print("gold", o)
