10aacc7696e8739276bea3ab9af69cf718eab755
XZETGEXI <77068772+XZETGEXI@users.noreply.github.com>
2021-01-29 13:50:55 +0100

    Update d5.py

--- 2017/d5.py
+++ 2017/d5.py
@@ -5,25 +5,28 @@ data = [datum.strip() for datum in data]
 
 data = [int(datum) for datum in data]
 
-# data = [0,3,0,1,-3]
+def main(star):
+	indice = 0
+	steps = 0
+	
+	d_n_off = {n: i for n, i in enumerate(data)}
+	
+	while True:
+		try:
+			offset = d_n_off[indice]
+			if star == "gold":
+				if offset < 3:
+					d_n_off[indice] += 1
+				else:
+					d_n_off[indice] -= 1
+			indice += offset
+			steps += 1
+			
+		except KeyError:
+			print("out")
+			break
 
-d_n_off = {n: i for n, i in enumerate(data)}
-
-indice = 0
-steps = 0
-
-while 1:
-    try:
-        offset = d_n_off[indice]
-        #gold
-        if offset < 3:
-            d_n_off[indice] += 1
-        else:
-            d_n_off[indice] -= 1
-        indice += offset
-        steps += 1
-    except KeyError:
-        print("out")
-        break
-
-print(steps)
+	print(star, steps)
+	
+main("silver")
+main("gold")
