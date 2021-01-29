0471c24474169af852179d6fbfd3ab2a016d47cf
XZETGEXI <77068772+XZETGEXI@users.noreply.github.com>
2021-01-29 13:50:16 +0100

    Update d3.py

--- 2017/d3.py
+++ 2017/d3.py
@@ -1,66 +1,67 @@
 from pprint import pprint
 import itertools as it
 
-# with open("d3.txt") as f:
-#     data  [datum.strip() for datum in f.readlines()]
+BIGVALUE = 123 # replace with your input
 
-# data  [datum.split() for datum in data]
-# data  [map(int, datum) for datum in data]
-# data  [[*datum] for datum in data] 312051
-
-# print(data)
-BIGVALUE = 312051
-
-d_coo_values = {}
 d_rose_func = {"E": lambda x,y: (x + 1, y),
 "N": lambda x,y: (x, y + 1),
 "W": lambda x,y: (x - 1, y),
 "S": lambda x,y: (x, y - 1),
 }
 
-value = 1
-value_coords = (0,0)
-times = 1
-turn = 0
-flag = False
+def change_gold(d_coo_values, v_c):
+	score = 0
+	for i,j in it.product((-1,0,1), repeat = 2):
+		if i == 0 and j == 0:
+			pass
+		try:
+			score += d_coo_values[(v_c[0] + i, v_c[1] + j)]
+		except:
+			pass
 
-d_coo_values[value_coords] = value
+	return score
 
-def cha(v, v_c):
-    score = 0
-    for i,j in it.product((-1,0,1), repeat = 2):
-        if i == 0 and j == 0:
-            pass
-        try:
-            score += d_coo_values[(v_c[0] + i, v_c[1] + j)]
-        except:
-            pass
+def main(star):
+	d_coo_values = {}
+	value = 1
+	value_coords = (0,0)
+	times = 1
+	turn = 0
+	flag = False
 
-    return score
+	d_coo_values[value_coords] = value
+	
+	for rose in it.cycle(d_rose_func):
+		
+		if turn == 2:
+			turn = 0
+			times += 1
+			
+		for i in range(times):
+			value_coords = d_rose_func[rose](*value_coords)
+			if star == "silver":
+				value += 1
+			if star == "gold":
+				value = change_gold(d_coo_values, value_coords)
+			
+			d_coo_values[value_coords] = value
+			
+			if value >= BIGVALUE:
+				flag = True
+				break
+		
+		if flag:
+			break
+		
+		turn += 1
+	
+	if star == "silver":
+		inverted = {v:k for k,v in d_coo_values.items()}
+		return sum(map(abs, inverted[BIGVALUE]))
+	if star == "gold":
+		return value
 
-for rose in it.cycle(d_rose_func):
-    
-    if turn == 2:
-        turn = 0
-        times += 1
-    for i in range(times):
-        value_coords = d_rose_func[rose](*value_coords)
-        value = cha(value, value_coords)
-        
-        d_coo_values[value_coords] = value
-        if value >= BIGVALUE:
-            flag = True
-            break
-    
-    if flag:
-        break
-    
-    turn += 1
 
-print(value)
+print("silver", main("silver"))
 
-# for k,v in d_coo_values.items():
-#     if v == BIGVALUE:
-#         print(k)
-        
-print(151 + 279)
+print("gold", main("gold"))
